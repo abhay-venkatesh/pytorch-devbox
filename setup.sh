0c3d700f9c26ab0bfd7cd435a4d7c019992fcd30
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-12-08 12:19:33 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
+# Create a conda environment
 conda create --name pytorch python=3.6
 source activate pytorch
+
+# Install PyTorch and TorchVision
 conda install pytorch torchvision -c pytorch
+
+# Install other packages
 conda install --file requirements.txt
+
+# Install COCO API
+conda install -c hcc pycocotools

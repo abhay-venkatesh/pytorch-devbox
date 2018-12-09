ba6a474921b7eee99034309465a4e9097afbba3d
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-12-09 15:17:06 -0600

    fix bug

--- sandbox/log_to_image.py
+++ sandbox/log_to_image.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import sys
+import tensorflow as tf
 
 if len(sys.argv) < 2:
-        print("Usage: python main.py <path to log file>")
-        exit()
-
+    print("Usage: python main.py <path to log file>")
+    exit()
 logfile_path = sys.argv[1]
 
 losses = []
@@ -12,7 +12,7 @@ for e in tf.train.summary_iterator(logfile_path):
     for v in e.summary.value:
         if v.tag == 'loss':
             losses.append(v.simple_value)
-            
+
 plt.plot(losses)
 plt.ylabel('Loss')
 plt.savefig('loss.png')

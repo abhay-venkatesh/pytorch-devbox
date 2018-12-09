d26407833867a2ea45d5441b1c0a9894b837ec57
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-12-09 15:16:11 -0600

    Log to image

---
+++ sandbox/log_to_image.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+if len(sys.argv) < 2:
+        print("Usage: python main.py <path to log file>")
+        exit()
+
+logfile_path = sys.argv[1]
+
+losses = []
+for e in tf.train.summary_iterator(logfile_path):
+    for v in e.summary.value:
+        if v.tag == 'loss':
+            losses.append(v.simple_value)
+            
+plt.plot(losses)
+plt.ylabel('Loss')
+plt.savefig('loss.png')
